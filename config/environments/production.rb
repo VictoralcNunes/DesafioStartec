3fbcff3b184d07504a9c33bf866dae4c9820ebe2
VictoralcNunes <victoralcnunes@hotmail.com>
2020-01-19 17:50:26 -0300

    Mailer is sending! (on prod env)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
+  
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -89,7 +90,20 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  # configuring the mailer
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://obscure-citadel-44753.herokuapp.com/'
+  config.action_mailer.default_url_options = { :host => host }
 
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "victoralcnunes@gmail.com",
+    :password             => "ruydmbwfowqelapv",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
